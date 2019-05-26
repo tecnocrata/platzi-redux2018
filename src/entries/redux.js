ddaecbb5904767ed414ba2f8b0be2c457ddcfb0b
enrique.ortuno <enrique@ortuno.net>
2019-05-25 20:07:07 -0400

    Configurando el estado inicial

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,3 +1,4 @@
+import { createStore } from "redux";
 const $form = document.getElementById("form");
 
 $form.addEventListener("submit", handleSubmit);
@@ -8,3 +9,21 @@ function handleSubmit(e) {
   const title = data.get("title");
   console.log(title);
 }
+
+const reducer = state => {
+  return state;
+};
+const initialState = [
+  {
+    title: "Despacito"
+  },
+  {
+    title: "Fast"
+  },
+  {
+    title: "One more time"
+  }
+];
+const enhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(reducer, initialState, enhancer);
