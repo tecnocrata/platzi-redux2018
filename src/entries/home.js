56902f21307d9a952a7ef737e9e40d11173a6dde
enrique.ortuno <enrique@ortuno.net>
2019-05-27 14:30:23 -0400

    Usando el store dentro de una pagina

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { hydrate } from 'react-dom';
-import Home from '../pages/containers/home';
+import React from "react";
+import { hydrate, render } from "react-dom";
+import Home from "../pages/containers/home";
 // import Playlist from './src/playlist/components/playlist';
-import data from '../api.json';
-// console.log('Hola mundo!' )
+import data from "../api.json";
 
-const homeContainer = document.getElementById('home-container')
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const reducer = state => {
+  return state;
+};
+const initialState = {
+  data: { ...data }
+};
+const enhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(reducer, initialState, enhancer);
+
+const homeContainer = document.getElementById("home-container");
 
 // ReactDOM.render(que voy a renderizar, donde lo haré);
 // const holaMundo = <h1>hola Estudiante!</h1>;
-hydrate( <Home data={data} />, homeContainer);
-
+hydrate(
+  <Provider store={store}>
+    <Home />
+  </Provider>,
+  homeContainer
+);
--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
-import HomeLayout from '../components/home-layout';
-import Categories from '../../categories/components/categories';
-import Related from '../components/related';
-import ModalContainer from '../../widgets/containers/modal';
-import Modal from '../../widgets/components/modal';
-import HandleError from '../../error/containers/handle-error';
-import VideoPlayer from '../../player/containers/video-player';
+import React, { Component } from "react";
+import HomeLayout from "../components/home-layout";
+import Categories from "../../categories/components/categories";
+import Related from "../components/related";
+import ModalContainer from "../../widgets/containers/modal";
+import Modal from "../../widgets/components/modal";
+import HandleError from "../../error/containers/handle-error";
+import VideoPlayer from "../../player/containers/video-player";
+import { connect } from "react-redux";
 
 class Home extends Component {
   state = {
-    modalVisible: false,
-  }
-  handleOpenModal = (media) => {
+    modalVisible: false
+  };
+  handleOpenModal = media => {
     this.setState({
       modalVisible: true,
       media
-    })
-  }
-  handleCloseModal = (event) => {
+    });
+  };
+  handleCloseModal = event => {
     this.setState({
-      modalVisible: false,
-    })
-  }
+      modalVisible: false
+    });
+  };
   render() {
     return (
       <HandleError>
         <HomeLayout>
           <Related />
           <Categories
-            categories={this.props.data.categories}
+            categories={this.props.categories}
             handleOpenModal={this.handleOpenModal}
           />
-          {
-            this.state.modalVisible &&
+          {this.state.modalVisible && (
             <ModalContainer>
-              <Modal
-                handleClick={this.handleCloseModal}
-              >
+              <Modal handleClick={this.handleCloseModal}>
                 <VideoPlayer
                   autoplay
                   src={this.state.media.src}
@@ -44,11 +42,17 @@ class Home extends Component {
                 />
               </Modal>
             </ModalContainer>
-          }
+          )}
         </HomeLayout>
       </HandleError>
-    )
+    );
   }
 }
 
-export default Home
+const mapStateToProps = (state, props) => {
+  return {
+    categories: state.data.categories
+  };
+};
+
+export default connect(mapStateToProps)(Home);
