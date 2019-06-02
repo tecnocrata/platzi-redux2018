44631d5e381a707377052a5f48fc5ae3dfdef8fc
enrique.ortuno <enrique@ortuno.net>
2019-06-02 00:29:45 -0400

    Creando un middleware basico

--- src/entries/home.js
+++ src/entries/home.js
@@ -4,7 +4,7 @@ import Home from "../pages/containers/home";
 // import Playlist from './src/playlist/components/playlist';
 //import data from "../api.json";
 //import normalized from "../schemas";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "../reducers";
 import { Map as map } from "immutable";
@@ -23,8 +23,21 @@ import { Map as map } from "immutable";
 //     mediaId: -1
 //   }
 // };
-const enhancer =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+//MIDDLEWARE
+const logger = ({ getState, dispatch }) => {
+  return next => {
+    return action => {
+      console.log("La accion es ", action);
+      //return next(action);
+      const result = next(action);
+      console.log("Nuevo estado, ", getState());
+    };
+  };
+};
+// const enhancer =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const enhancer = applyMiddleware(logger);
 const store = createStore(rootReducer, map(), enhancer);
 
 const homeContainer = document.getElementById("home-container");
